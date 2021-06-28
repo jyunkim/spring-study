c7e8060671d6bc3a6f050ddc191b9458e5abb4c6
JyunKim <kann1997@naver.com>
2021-06-29 01:09:35 +0900

    Initial commit

---
+++ src/test/java/jihyun/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jihyun.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
