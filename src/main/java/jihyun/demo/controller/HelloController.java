139858b0e7ee31af5bd0831df9d35749d114d60e
JyunKim <kann1997@naver.com>
2021-06-30 16:46:50 +0900

    (feat): Add several types of response controller

--- src/main/java/jihyun/demo/controller/HelloController.java
+++ src/main/java/jihyun/demo/controller/HelloController.java
@@ -3,14 +3,52 @@ package jihyun.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    // GET 요청 라우팅
+    // GET 요청 라우팅 annotation
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello");  // 템플릿 변수 설정
         return "hello";  // resources/templates/hello.html render
+    }
+
+    // Query string(default - required=true)
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("key") String value, Model model) {
+        model.addAttribute("key", value);
+        return "hello-mvc";
+    }
+
+    // HTTP 응답 body에 string 반환
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString() {
+        return "Hello Spring";
+    }
+
+    // HTTP 응답 body에 객체를 JSON 형태로 반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    // 내부 클래스
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
