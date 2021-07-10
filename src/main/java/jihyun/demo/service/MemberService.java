4a5a72977030519b7db0fc2f6f3c13100e416d99
JyunKim <kann1997@naver.com>
2021-07-11 01:26:48 +0900

    feat: Add member service

---
+++ src/main/java/jihyun/demo/service/MemberService.java
@@ -0,0 +1,40 @@
+package jihyun.demo.service;
+
+import jihyun.demo.domain.Member;
+import jihyun.demo.repository.MemberRepository;
+import jihyun.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름이 있으면 안됨
+        // 반환값이 null이 아닐 경우 예외
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
