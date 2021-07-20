c36b3249ed5acd4c4ea6f560d23ea991c3ee016b
JyunKim <kann1997@naver.com>
2021-07-20 16:56:12 +0900

    feat: Add member form pages

--- src/main/java/jihyun/demo/controller/MemberController.java
+++ src/main/java/jihyun/demo/controller/MemberController.java
@@ -1,8 +1,14 @@
 package jihyun.demo.controller;
 
+import jihyun.demo.domain.Member;
 import jihyun.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 스프링 빈으로 등록
 @Controller
@@ -16,5 +22,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        // 리다이렉션
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/jihyun/demo/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package jihyun.demo.controller;
+
+public class MemberForm {
+    // Post form의 key값인 name의 value가 변수 name에 들어감
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
