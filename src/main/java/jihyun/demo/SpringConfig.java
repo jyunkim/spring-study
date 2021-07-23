d90c6a9b041b376ed17326ba42dee6fe9a5abcf3
JyunKim <kann1997@naver.com>
2021-07-23 16:31:46 +0900

    feat: Apply spring data jpa

--- src/main/java/jihyun/demo/SpringConfig.java
+++ src/main/java/jihyun/demo/SpringConfig.java
@@ -1,14 +1,10 @@
 package jihyun.demo;
 
-import jihyun.demo.repository.JdbcTemplateMemberRepository;
-import jihyun.demo.repository.JpaMemberRepository;
 import jihyun.demo.repository.MemberRepository;
 import jihyun.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -20,23 +16,30 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private final EntityManager em;
+//    private final EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
